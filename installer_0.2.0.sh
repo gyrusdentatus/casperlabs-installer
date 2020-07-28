67b3642eed33b29c774ca89921a18c8474ee05a1
Hans Bricks <33793809+gyrusdentatus@users.noreply.github.com>
2020-07-28 23:14:25 +0200

    Update installer_0.2.0.sh
    added usage and flags as well as input for user's private key

--- installer_0.2.0.sh
+++ installer_0.2.0.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
+if [[ $USER != "root" ]]
+then
+  printf "%b\n\n\n" "${WHITE} This script must be run as ${YELLOW} root ${WHITE} or with ${YELLOW} sudo!"
+  exit 1
+fi
+
+function display_usage() {
+	#printf "%b\n\n\n" "${WHITE}This script must be run with super-user privileges."
+	#echo -e "\nUsage:\n__g5_token5eefd24a11c4a [arguments] \n"
+
+
+      cat 1>&2 <<EOF
+casperlabs_install.sh 0.2.0 (2020-28-07)
+The installer and launcher for casperlabs validator-node
+
+USAGE:
+    ./installer_0.2.0.sh [FLAGS]
+
+FLAGS:
+    -i  --install           Full installation and setup
+    -k  --keys              Create new set of keys
+    -p  --private-key       Enter the private key
+    -r, --run               Start the node without installation
+    -h, --help              Prints help information
+    -V, --version           Prints version information
+    -s  --status            Prints status of the node
+    -f  --firewall          Firewall setup
+    -n  --print-node        Create casperlabs-node.service for systemd
+    -e  --print-engine      Create casperlabs-engine-grpc.service for systemd
+
+EOF
+}
+
 ## Colours variables for the installation script
 RED='\033[1;91m' # WARNINGS
 YELLOW='\033[1;93m' # HIGHLIGHTS
@@ -89,7 +122,7 @@ function add_repos() {
      printf "%b\n\n\n" "${WHITE} Updating apt sources ..."
      apt update > /dev/null 2>&1
      printf "%b\n\n\n" "${WHITE} Installing casperlabs and casperlabs-client ..."
-     apt install casperlabs casperlabs-client -y > /dev/null 2>&1 && printf "%b\n\n\n" "${WHITE} Casperlabs successfully downloaded and installed ..." 
+     apt install casperlabs casperlabs-client -y > /dev/null 2>&1 && printf "%b\n\n\n" "${WHITE} Casperlabs successfully downloaded and installed ..."
    else
      printf "%b\n\n\n" "${WHITE} Casperlabs are already in your apt sources.list.d/ directory ..."
      printf "%b\n\n\n" "${WHITE} Skipping ..."
@@ -102,9 +135,9 @@ function create_config() {
         printf '%s\n' " [server] " > /etc/casperlabs/config.toml
         printf '%s\n' " host=${ip_addr} " >> /etc/casperlabs/config.toml
         cat config.toml >> /etc/casperlabs/config.toml
-    else    
-        printf "%b\n\n\n" "${WHITE} config.toml already exists in ${YELLOW} /etc/casperlabs/ ${WHITE} skipping ..." 
- fi 
+    else
+        printf "%b\n\n\n" "${WHITE} config.toml already exists in ${YELLOW} /etc/casperlabs/ ${WHITE} skipping ..."
+ fi
 }
 
 function systemd_print_node() {
@@ -148,8 +181,8 @@ function systemd_print_engine () {
         printf "%b\n\n\n" "${WHITE} --------------------------------------------------------------------------------"
         printf "%b\n\n\n" "${YELLOW} Creating ${WHITE} a systemd service file to run casperlabs-engine-grpc-server.service in the background: "
         printf "%b\n\n\n"
-    
-    
+
+
         printf '%s\n' "[Unit]" > /etc/systemd/system/casperlabs-engine-grpc-server.service
         printf '%s\n' "Description=CasperLabs Engine GRPC Server" >> /etc/systemd/system/casperlabs-engine-grpc-server.service
         printf '%s\n' "After=network.target" >> /etc/systemd/system/casperlabs-engine-grpc-server.service
@@ -178,6 +211,12 @@ function systemd_print_engine () {
   fi
 }
 
+function () priv_key {
+  printf "%b\n\n\n" "${WHITE} Enter your ${YELLOW} private key ${WHITE}. It will be moved to your keys directory."
+  read priv_key
+  printf "%b\n\n\n" "${WHITE} You have entered $priv_key as your private validator key."
+  echo "${priv_key}" > $HOME/.casperlabs/casperlabs/keys/validator-private1.key && printf "%b\n\n\n" "${WHITE} Your private key was ${LGREEN} successfully created! ${WHITE}"
+}
 
 function create_keys () {
   keys_directory=$HOME/.casperlabs/casperlabs/keys
@@ -202,11 +241,69 @@ casper_ufw
 systemd_print_node
 systemd_print_engine
 get_chainspec
+priv_key
 create_keys
 cd $HOME/casperlabs-installer
 create_config
 chown -R casperlabs:casperlabs $HOME/.casperlabs/
 chown casperlabs:casperlabs /etc/casperlabs/config.toml
 systemctl start casperlabs-node.service && systemctl start casperlabs-engine-grpc-server.service
-} 
-full_setup
+printf "%b\n\n\n" "${WHITE} Your node should be now running. Check status after a few seconds with the -s flag ..."
+}
+
+# Full installation
+if [[ ("$1" = "--install") ||  "$1" = "-i" ]]
+then
+  full_setup
+fi
+## Create the systemd.service file
+if [[ ("$1" = "--print-node") ||  "$1" = "-n" ]]
+then
+  systemd_print_node
+fi
+##  Create the systemd.service file locally
+if [[ ("$1" = "--print-engine") ||  "$1" = "-e" ]]
+then
+  systemd_print_engine
+fi
+## Run the node
+if [[ ("$1" = "--run") ||  "$1" = "-r" ]]
+then
+  systemctl start casperlabs-node.service
+fi
+## Get status from the systemdaemon file
+if [[ ("$1" = "--status") ||  "$1" = "-s" ]]
+then
+  systemctl status casperlabs-node.service
+fi
+
+if [[ ("$1" = "--private-key") ||  "$1" = "-p" ]]
+then
+  priv_key
+fi
+
+## Setup the firewall
+if [[ ("$1" = "--firewall") ||  "$1" = "-f" ]]
+then
+  casper_ufw
+fi
+## If no arguments supplied, display usage
+if [ -z "$1" ]
+then
+  display_usage
+fi
+
+## Check whether user had supplied -h or --help . If yes display usage
+if [[ ("$1" = "--help") ||  "$1" = "-h" ]]
+#if [[ ( $# == "--help") ||  $# == "-h" ]]
+then
+  display_usage
+  exit 0
+fi
+## Prints the version of Nym used
+if [[ ("$1" = "--version") ||  "$1" = "-V" ]]
+#if [[ ( $# == "--help") ||  $# == "-h" ]]
+then
+  display_usage
+  exit 0
+fi
